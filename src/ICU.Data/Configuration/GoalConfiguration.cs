b102ba7afafd609add504542bd9bbc02777027eb
Iustin Trotus <trotusiustin@gmail.com>
2021-02-22 10:46:44 +0000

    PAtient and Goal db config

---
+++ src/ICU.Data/Configuration/GoalConfiguration.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ICU.Data.Models;
+
+namespace ICU.Data.Configuration
+{
+    public class GoalConfiguration : IEntityTypeConfiguration<Goal>
+    {
+        public void Configure(EntityTypeBuilder<Goal> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(w => w.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.HasOne(p => p.Patient)
+                .WithMany()
+                .HasForeignKey(p => p.PatientId);
+
+            builder.Property(p => p.Value)
+                .IsRequired()
+                .HasMaxLength(450);
+
+            builder.Property(p => p.IsMainGoal)
+                .IsRequired();
+
+            builder.HasIndex(p => new { p.PatientId, p.IsMainGoal })
+                .IsUnique(false);
+        }
+
+    }
+}
---
+++ src/ICU.Data/Configuration/PatientConfiguration.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ICU.Data.Models;
+
+namespace ICU.Data.Configuration
+{
+    public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+    {
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(w => w.Id)
+                .IsRequired()
+                .UseIdentityColumn()
+                .ValueGeneratedOnAdd();
+
+            builder.Property(p => p.PhoneNumber)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(450);
+
+            builder.Property(p => p.Ward)
+                .IsRequired()
+                .HasMaxLength(450);
+
+            builder.Property(p => p.Hospital)
+                .IsRequired()
+                .HasMaxLength(450);
+
+            builder.Property(p => p.AdmissionDate)
+                .IsRequired();
+        }
+
+    }
+}
