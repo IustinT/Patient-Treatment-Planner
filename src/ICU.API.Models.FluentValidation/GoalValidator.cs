2e0761adb7f2d6f392713460a28ef72222928c5c
Iustin Trotus <trotusiustin@gmail.com>
2021-02-22 10:46:20 +0000

    API model validations for Patient and Goal

---
+++ src/ICU.API.Models.FluentValidation/GoalValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+using ICU.Data.Models;
+
+using System;
+
+namespace ICU.API.Models.FluentValidation
+{
+    public class GoalValidator : AbstractValidator<Goal>
+    {
+        public GoalValidator()
+        {
+            RuleFor(p => p.PatientId)
+                .NotEmpty();
+
+            RuleFor(p => p.Value)
+                .NotEmpty()
+                .Length(2, 450);
+
+            RuleFor(p => p.IsMainGoal)
+                .NotNull();
+
+        }
+    }
+}
---
+++ src/ICU.API.Models.FluentValidation/PatientValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+
+using ICU.Data.Models;
+
+using System;
+
+namespace ICU.API.Models.FluentValidation
+{
+    public class PatientValidator : AbstractValidator<Patient>
+    {
+        public PatientValidator()
+        {
+            RuleFor(patient => patient.PhoneNumber)
+              .NotEmpty()
+              .Length(11);
+
+            RuleFor(patient => patient.Name)
+                .NotEmpty()
+                .Length(2, 450);
+
+            RuleFor(patient => patient.AdmissionDate)
+                .NotNull()
+                .LessThanOrEqualTo(DateTime.Now);
+
+            RuleFor(patient => patient.Hospital)
+                .NotEmpty()
+                .Length(2, 450);
+
+            RuleFor(patient => patient.Ward)
+                .NotEmpty()
+                .Length(2, 450);
+
+        }
+
+
+    }
+}
