66cb158f88ab7dd61101cf095cf80a709bd69137
Iustin Trotus <trotusiustin@gmail.com>
2021-02-22 10:47:01 +0000

    db migrations

---
+++ src/ICU.Data/IcuContext.cs
@@ -0,0 +1,44 @@
+﻿using ICU.Data.Configuration;
+using ICU.Data.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Reflection;
+
+/*
+    EntityFrameworkCore\Add-Migration -Name Initial -Context ICU.Data.IcuContext -StartupProject ICU.API -Project ICU.Data
+
+    EntityFrameworkCore\Remove-Migration -Context ICU.Data.IcuContext -StartupProject ICU.API -Project ICU.Data
+
+    EntityFrameworkCore\Update-Database -Context ICU.Data.IcuContext -StartupProject ICU.API -Project ICU.Data
+
+ */
+
+namespace ICU.Data
+{
+    public class IcuContext : DbContext
+    {
+        static readonly Assembly assembly;
+
+        static IcuContext()
+        {
+            assembly = Assembly.GetAssembly(typeof(IcuContext));
+        }
+
+        public IcuContext(DbContextOptions<IcuContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Goal> Goals { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+        }
+    }
+}
