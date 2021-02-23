e6cbbe95c68d71086d0374646bd7cc889c308dd8
Iustin Trotus <trotusiustin@gmail.com>
2021-02-23 09:31:46 +0000

    added system config controller

---
+++ src/ICU.API/Controllers/SystemConfigController.cs
@@ -0,0 +1,33 @@
+﻿using ICU.Data;
+using ICU.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ICU.API.Controllers
+{
+    [Route("[controller]")]
+    [ProducesResponseType(typeof(IEnumerable<SystemConfig>), StatusCodes.Status200OK)]
+    public class SystemConfigController : BaseController
+    {
+        public SystemConfigController(IcuContext context) : base(context)
+        { }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var configData = new SystemConfig
+            {
+                ImageCategories = await Context.ImageCategories.ToListAsync(),
+
+            };
+
+            return Ok(configData);
+        }
+    }
+}
