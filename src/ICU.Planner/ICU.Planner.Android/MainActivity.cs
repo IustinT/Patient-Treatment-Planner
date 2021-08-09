52aa33db3d679c7f67016a90684e8b3b12026e26
Iustin Trotus <iustin@trotus.co.uk>
2021-08-09 19:12:06 +0100

    Update MainActivity.cs

--- src/ICU.Planner/ICU.Planner.Android/MainActivity.cs
+++ src/ICU.Planner/ICU.Planner.Android/MainActivity.cs
@@ -1,15 +1,14 @@
 ﻿using Android.App;
-
 using Android.Content.PM;
 using Android.OS;
-
 using Prism;
 using Prism.Ioc;
 
 namespace ICU.Planner.Droid
 {
     [Activity(Theme = "@style/MainTheme",
-              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode
+                               | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -35,4 +34,3 @@ namespace ICU.Planner.Droid
         }
     }
 }
-
